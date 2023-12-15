2525c1952309f742c98e0e5f4061e4452375772c
Leonardo Machado <leonardomachado200112@gmail.com>
2023-12-15 02:11:48 -0300

    axios config and starting the event page

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -6,9 +6,15 @@ interface Props {
 
 const PageEvent = ({ params }: Props) => {
     return (
-        <div>
-            ID DO EVENTO: {params.id}
-        </div>
+        <main className="text-center mx-auto mas-w-lg p-5s">
+            <header>
+                <h2 className="text-2xl text-yellow-400">Amigo Secreto</h2>
+                <h1 className="text-3xl mt-5 mb-2">**TITULO**</h1>
+                <p className="text-sm mb-5">**Descrição**</p>
+            </header>
+
+            <footer className="mt-5 text-sm">Criado por alguem</footer>
+        </main>
     )
 }
 
