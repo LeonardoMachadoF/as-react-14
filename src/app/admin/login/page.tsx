8004e753c7f1eed29c54a02a2e17f0eccc335a65
Leonardo Machado <leonardomachado200112@gmail.com>
2024-01-14 18:56:51 -0300

    creating InputField component and using it on admin login page

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,6 +1,22 @@
+"use client"
+
+import { InputField } from "@/components/admin/InputField";
+import { useState } from "react";
+
 function Page() {
+    const [passwordInput, setPasswordInput] = useState("");
+
+
     return (
-        <div>PAINEL ADMIN - Login</div>
+        <div>
+            PAINEL ADMIN - Login
+            <InputField
+                type="password"
+                value={passwordInput}
+                onChange={e => setPasswordInput(e.target.value)}
+                placeholder="Digite a senha"
+            />
+        </div>
     )
 }
 
