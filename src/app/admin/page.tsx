2da0c8d276bb4b729d7c20ee6304ee6414b6f3ae
Leonardo Machado <leonardomachado200112@gmail.com>
2024-01-13 22:50:59 -0300

    cookies-next and auth verification on admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,4 +1,10 @@
-function Page() {
+import * as api from "@/api/server";
+import { redirect } from "next/navigation";
+
+async function Page() {
+    const logged = await api.pingAdmin();
+
+    if (!logged) return redirect("/admin/login");
     return (
         <div>PAINEL ADMIN</div>
     )
