a6bf13ac417daf0c17251356df0e1676ea93f751
Leonardo Machado <leonardomachado200112@gmail.com>
2024-01-16 18:29:56 -0300

    login function

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -3,27 +3,52 @@
 import { Button } from "@/components/admin/Button";
 import { InputField } from "@/components/admin/InputField";
 import { useState } from "react";
+import * as api from "@/api/admin";
+import { setCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
 
 function Page() {
+    const router = useRouter();
     const [passwordInput, setPasswordInput] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [warning, setWarning] = useState("");
 
-    function handleLoginButton() {
+    async function handleLoginButton() {
+        if (passwordInput) {
+            setWarning("");
+            setLoading(true);
+            const token = await api.login(passwordInput);
+            setLoading(false);
 
+            if (!token) {
+                setWarning("Acesso negado!")
+            } else {
+                setCookie('token', token);
+                router.push('/admin');
+            }
+        }
     }
 
     return (
         <div>
-            PAINEL ADMIN - Login
+            <p className="text-center py-4">Qual a senha secreta?</p>
+            <div className="mx-auto max-w-lg"></div>
+
             <InputField
                 type="password"
                 value={passwordInput}
                 onChange={e => setPasswordInput(e.target.value)}
                 placeholder="Digite a senha"
+                disabled={loading}
             />
             <Button
-                value="Entrar"
+                value={loading ? "Carregando..." : "Entrar"}
                 onClick={handleLoginButton}
+                disabled={loading}
             />
+            {warning &&
+                <div className="border border-dashed border-gray-400 p-3">{warning}</div>
+            }
         </div>
     )
 }
